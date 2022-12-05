8ac5b9f52f9fefe292ab190670e1ee8f51053473
Jeevak Lonare <lonarejeevak@gmail.com>
2022-12-05 18:58:20 +0530

    Program for converting Years To Days Negative No's don't work.

---
+++ src/com/bootcoding/java/CheckValue.java
@@ -0,0 +1,14 @@
+package com.bootcoding.java;
+
+public class CheckValue {
+
+    public static void main(String[] args) {
+      boolean result = checkValue(0.2,50,9);
+        System.out.println(result);
+    }
+        public static boolean checkValue(double prob, int prize, int pay){
+        return prob*prize>pay;
+        }
+    }
+
+
