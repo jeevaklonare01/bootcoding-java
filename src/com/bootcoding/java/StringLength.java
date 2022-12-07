1fe47a5accb6764b32b38638c2ee63ad598074d6
Jeevak Lonare <lonarejeevak@gmail.com>
2022-12-07 21:58:49 +0530

    Program to Add Elements of array.

---
+++ src/com/bootcoding/java/StringLength.java
@@ -0,0 +1,16 @@
+package com.bootcoding.java;
+
+public class StringLength {
+    public static void main(String[] args) {
+        String str1 = "i love java";
+        String str2 = "I love to code";
+        boolean result = findTotalNumber(str1,str2);
+        System.out.println(result);
+    }
+    public static boolean findTotalNumber(String s1, String s2){
+        if(s1.equals(s2)){
+            return(true);
+        }
+        return(false);
+    }
+}
