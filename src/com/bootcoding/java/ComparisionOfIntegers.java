dcdd57ddf46533b4f9947d5266443a40f82cabe1
Jeevak Lonare <lonarejeevak@gmail.com>
2022-12-07 22:10:06 +0530

    Program to compare getReturnValue of sum or either the main value of the integers.

---
+++ src/com/bootcoding/java/ComparisionOfIntegers.java
@@ -0,0 +1,11 @@
+package com.bootcoding.java;
+
+public class ComparisionOfIntegers {
+    public static void main(String[] args) {
+        boolean result = getReturnValue(3,7);
+        System.out.println(result);
+    }
+    public static boolean getReturnValue(int a , int b){
+        return ((a + b) == 10 || a == 10 || b == 10);
+    }
+}
