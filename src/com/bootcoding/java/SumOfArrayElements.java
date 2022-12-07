6296ea6f9f39580b886a4c9996f577eef8b9c2b1
Jeevak Lonare <lonarejeevak@gmail.com>
2022-12-07 21:33:25 +0530

    Program to Add Elements of array.

--- src/com/bootcoding/java/SumOfArrayElements.java
+++ src/com/bootcoding/java/SumOfArrayElements.java
@@ -3,10 +3,9 @@ package com.bootcoding.java;
 public class SumOfArrayElements {
     public static void main(String[] args) {
             int arr[]={10,20};
-        int res = calculateSumOfArray(arr);
+        int res = calculateSumOfArray(arr,0);
     }
-    public static int calculateSumOfArray(int[] array){
-        int sum=0;
+    public static int calculateSumOfArray(int[] array,int sum){
         for (int i =0; i<array.length; i++) {
             if (array[i] > 0) {
                 sum += array[i];
