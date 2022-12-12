2d5f9608a0307c5a0fb066dd34a3e497082a672d
Jeevak Lonare <lonarejeevak@gmail.com>
2022-12-12 09:27:37 +0530

    Final Keyword

---
+++ src/com/bootcoding/java/Keyword/FinalKeyword.java
@@ -0,0 +1,23 @@
+package com.bootcoding.java.Keyword;
+
+import com.bootcoding.java.OPPs.Marker;
+
+public class FinalKeyword {
+    public static void main(String[] args) {
+        /*
+        * final keyword is used to create constant variable.
+        * final variable can not be modified once it is initialized
+         */
+        final int a = 10;
+        //a=20; not allowed
+        //a=30; not allowed
+        final double PI = 3.14;
+        //PI = 2.14 not allowed
+//        final keyword to object (reference variable)
+        final Marker blueMaker = new Marker();
+//        blueMaker=new Marker(); not allowed
+//        It is allowed to change properties of an object
+        blueMaker.color = "blue";
+        blueMaker.color = "red";
+    }
+}
