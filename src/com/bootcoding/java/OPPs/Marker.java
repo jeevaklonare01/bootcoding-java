3c287e915fadb73fb35c632400ebcd40bddca2af
Jeevak Lonare <lonarejeevak@gmail.com>
2022-12-12 08:22:08 +0530

    Marker

---
+++ src/com/bootcoding/java/OPPs/Marker.java
@@ -0,0 +1,28 @@
+package com.bootcoding.java.OPPs;
+
+public class Marker {
+    //Data Member
+    String type;
+    String brand;
+    String color;
+    double price;
+
+    //Member Methods
+
+    /**
+     Documentation comment
+     */
+
+    public void refill(){
+
+    }
+    public void write(String message){
+        System.out.println("On White Board - " + message);
+    }
+    public void print(){
+        System.out.println("Marker Brand : "+ brand);
+        System.out.println("Marker type : "+ type);
+        System.out.println("Marker Color : "+ color);
+        System.out.println("Marker Price : "+ price);
+    }
+}
