3cf0b42153cb7e7a18559ee9cfb6f9e9a2984c63
Jeevak Lonare <lonarejeevak@gmail.com>
2023-03-24 14:40:59 +0530

    -

---
+++ src/com/bootcoding/java/OPPs/Collection/list/LinkedListExample1.java
@@ -0,0 +1,33 @@
+package com.bootcoding.java.OPPs.Collection.list;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListExample1 {
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<String>();
+        list.add("Raju");
+        list.add("Banti");
+        list.add("Babli");
+        list.add("Babu");
+        list.add("Babu");
+        list.add("Babu");
+        list.add("Jai");
+        list.add("Raju");
+        //traditional loop
+        /*for (int i = 0 ; i< list.size();i++){
+            System.out.println(list.get(i));
+        }*/
+        //for each loop
+        /*for (String str:list){
+            System.out.println(str);
+        }*/
+        //Using Iterator
+        /*
+        Iterator<String> stringIterator = list.iterator();
+            while(stringIterator.hasNext()){
+                System.out.println(stringIterator.next());
+            }*/
+        list.forEach(s -> System.out.println(s));
+        }
+}
---
+++ src/com/bootcoding/java/OPPs/Collection/list/LinkedListExample2.java
@@ -0,0 +1,30 @@
+package com.bootcoding.java.OPPs.Collection.list;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListExample2 {
+    public static void main(String[] args) {
+        List<Person> list = new LinkedList<Person>();
+        list.add(new Person("Raja",22,2633193));
+        list.add(new Person("Banti",31,1234567));
+        list.add(new Person("Babli",23,7654321));
+        list.add(new Person("Babu",42,76543102));
+        //traditional loop
+        /*        for(int i=0;i< list.size();i++){
+            System.out.println(list.get(i));
+        }*/
+
+        //for each loop
+        /*for(Person person:list){
+            System.out.println(person);
+        }*/
+
+        //Using Iterator
+        /*Iterator<Person> personIterator = list.iterator();
+        while(personIterator.hasNext()){
+            System.out.println(personIterator.next());
+        }*/
+
+        list.forEach(person -> System.out.println(person));
+    }
+}
